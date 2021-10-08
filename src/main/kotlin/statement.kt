f3e890ba8ecd43464a8cf3c07ba1209830742bb1
Vitaly Bragilevsky <bravit111@gmail.com>
2021-10-08 10:55:27 +0300

    split calculators by play types

--- src/main/kotlin/statement.kt
+++ src/main/kotlin/statement.kt
@@ -51,30 +51,40 @@ private fun renderToPlainText(statementData: StatementData): String {
 
 private fun asUSD(thisAmount: Int): String? = NumberFormat.getCurrencyInstance(Locale.US).format(thisAmount / 100)
 
+val additionalVolumeCreditCalculator = mapOf(
+    PlayType.COMEDY to ::calculateComedyAddVolumeCredit
+)
+
 private fun volumeCreditsFor(perf: Performance, play: Play): Int {
-    var result = max(perf.audience - 30, 0)
-    // add extra credit for every five comedy attendees
-    if (play.type == PlayType.COMEDY)
-        result += floor(perf.audience / 5.0).toInt()
+    return max(perf.audience - 30, 0) +
+            (additionalVolumeCreditCalculator[play.type]?.invoke(perf) ?: 0)
+}
+
+private fun calculateComedyAddVolumeCredit(perf: Performance) = floor(perf.audience / 5.0).toInt()
+
+val amountCalculator = mapOf(
+    PlayType.TRAGEDY to ::calculateTragedyAmount,
+    PlayType.COMEDY to ::calculateComedyAmount
+)
+
+private fun amountFor(perf: Performance, play: Play): Int {
+    return amountCalculator[play.type]?.invoke(perf) ?:
+        throw Exception("Undefined amount calculator")
+}
+
+private fun calculateComedyAmount(perf: Performance): Int {
+    var result = 30000
+    if (perf.audience > 20) {
+        result += 10000 + 500 * (perf.audience - 20)
+    }
+    result += 300 * perf.audience
     return result
 }
 
-private fun amountFor(perf: Performance, play: Play): Int {
-    var result: Int
-    when (play.type) {
-        PlayType.TRAGEDY -> {
-            result = 40000
-            if (perf.audience > 30) {
-                result += 1000 * (perf.audience - 30)
-            }
-        }
-        PlayType.COMEDY -> {
-            result = 30000
-            if (perf.audience > 20) {
-                result += 10000 + 500 * (perf.audience - 20)
-            }
-            result += 300 * perf.audience
-        }
+private fun calculateTragedyAmount(perf: Performance): Int {
+    var result = 40000
+    if (perf.audience > 30) {
+        result += 1000 * (perf.audience - 30)
     }
     return result
 }
